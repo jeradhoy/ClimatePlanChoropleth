273de8d96fe20834a51f367c41ffdfbc9a8b9035
jeradhoy <jeradhoy@gmail.com>
2015-08-12 11:31:26 -0600

    initial commit

---
+++ global.R
@@ -0,0 +1,96 @@
+#choropleth global file
+
+library(leaflet)
+
+#Includes district of columbia in state name vector
+new.state.names <- c(state.name[1:8],"District of Columbia", state.name[9:50])
+
+
+states.co2 = c()
+
+
+all.states.co2 <- read.csv(file="CO2FFC_2012.csv", header=FALSE)[4:309,3:25]
+
+for (i in 1:length(new.state.names)){
+    states.co2[[new.state.names[i]]] <- all.states.co2[(1:6+6*(i-1)),]
+    colnames(states.co2[[i]]) <- c(1990:2012)
+    rownames(states.co2[[i]]) <- c("Total", paste(read.csv(file="CO2FFC_2012.csv", header=FALSE)[5:9,2]))
+}
+
+pop <- read.csv(file="NST-EST2014-01_thousand.csv")[9:59,4:8]
+rownames(pop) <- new.state.names
+colnames(pop) <- c(2010, 2011, 2012, 2013, 2014)
+
+
+
+state.data <- data.frame(goals = numeric(51), newGoal = numeric(51), co2PerCapita = numeric(51), co2PerCapitaNew = numeric(51), total = numeric(51), commSlider = numeric(51), industSlider = numeric(51), residSlider = numeric(51), transSlider = numeric(51), elecSlider = numeric(51), totalNew = numeric(51), row.names=new.state.names)
+
+for (state in rownames(state.data)) {
+    state.data[state,5:10] <- c(states.co2[[state]][,"2012"])
+    state.data[state,c("goals", "newGoal")] <- 28-(1-states.co2[[state]][1,"2012"]/states.co2[[state]][1,"2005"])*100
+}
+
+state.data_2005 <- data.frame(total = numeric(51), row.names=new.state.names)
+
+for (state in rownames(state.data_2005)) {
+    state.data_2005[state,1] <- c(states.co2[[state]][1,"2005"])
+}
+
+usGoal <- 28-(1-sum(state.data[c(-1,-12),5])/sum(state.data_2005[c(-1,-12),]))*100
+
+
+
+
+state.data$co2PerCapita <- state.data[,5]/pop[,"2012"]*1e6
+state.data$co2PerCapitaNew <- state.data[,5]/pop[,"2012"]*1e6
+state.data$totalNew <- state.data$total
+
+state.data <- round(state.data, digits=4)
+
+densityBreaks <- c(-100, seq(0.001, 32.5), seq(34.5, 52, 2), seq(52, 117, 5))
+
+densityBreaks1 <- c(-100, seq(0.001, 32.5), seq(34.5, 52, 2), seq(52, 117, 5))
+densityBreaks2 <- c(-100, seq(0.001, 198, 2), seq(200, 240, 10), seq(260, 680, 20))
+
+colfunc <- colorRampPalette(c("white","yellow", "red", "darkred","black"))
+
+perCapitaPalette <- c("#66CD00", c(colfunc(length(densityBreaks1)-1)))
+totalPalette <- c("#66CD00", c(colfunc(length(densityBreaks2)-1)))
+
+## Assign colors to states
+    
+getColorsPerCapita <- function(goals, x){
+    colors <- structure(
+      perCapitaPalette[cut(goals[,1], densityBreaks1)],
+      names = tolower(rownames(goals))
+    )
+    return(colors[[x]])
+}
+
+getColorsTotal <- function(goals, x){
+    colors <- structure(
+    totalPalette[cut(goals[,1], densityBreaks2)],
+    names = tolower(rownames(goals))
+    )
+    return(colors[[x]])
+}
+
+# The state names that come back from the maps package's state database has
+# state:qualifier format. This function strips off the qualifier.
+getStateName <- function(id) {
+  strsplit(id, ":")[[1]][1]
+}
+
+simpleCap <- function(x) {
+    s <- strsplit(x, " ")[[1]]
+    paste(toupper(substring(s, 1,1)), substring(s, 2),
+    sep="", collapse=" ")
+}
+
+substrLeft <- function(x, n){
+    substr(x, 1, nchar(x)-n)
+}
+
+#library(shiny); wd <- "/Users/hoy/Desktop"; setwd(wd); runApp("choropleth")
+#sudo rm -rf /srv/shiny-server/choropleth
+#sudo cp -r /Users/hoy/Desktop/choropleth /srv/shiny-server/choropleth
---
+++ ui.R
@@ -0,0 +1,178 @@
+#global.R script for choropleth
+
+library(shiny)
+library(leaflet)
+library(rCharts)
+library(ShinyDash)
+library(shinyBS)
+
+#options(RCHART_LIB = 'NVD3')
+
+shinybootstrap2::withBootstrap2({
+
+shinyUI(fluidPage(title="Poulter Lab | State Emissions Reduction Explorer",
+
+tags$head(HTML('
+<meta property="og:url" content="http://bit.ly/1vtDKlh" />
+<meta property="og:title" content="Poulter Lab Emissions Reduction Explorer" />
+<meta property="og:description" content="Learn how you can reduce state CO2 emissions to reach Obama&#39;s emissions goal!" />
+<meta property="og:image" content="http://153.90.240.137:3838/choropleth/ChoroplethScreenshot1.png" />'
+)),
+
+tags$head(
+# Include our custom CSS
+includeCSS("styles.css"),
+HTML("
+
+<div id='fb-root'></div>
+<script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<script>
+ /*svg { -webkit-touch-callout: none; -webkit-user-select: none; -khtml-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; display: block; width:100%; height:100%; } svg text { font: normal 12px Arial; } svg .title { font: bold 14px Arial; } */
+ </script>
+
+)"
+
+),
+
+
+# Add a little CSS to make the map background pure white
+tags$style(type="text/css", "
+#showcase-code-position-toggle, #showcase-sxs-code { display: none; }
+
+.floater { background-color: white; padding: 8px; opacity: .9; border-radius: 6px; box-shadow: 0 0 15px rgba(0,0,0,0.2);}
+
+#.floater1 { background-color: NULL; padding: 8px; opacity: .9; border-radius: 6px; box-shadow: 0 0 15px rgba(0,0,0,0.2); }
+.clearBox { background-color: NULL; opacity: .1;}
+.infoBox { background-color: white; padding: 8px; opacity: 1; border-radius: 6px; box-shadow: 0 0 15px rgba(0,0,0,0.2); }
+.socialBox { background-color: clear; padding: 0px; opacity: 1; border-radius: 0px;}
+.shiny-output-error { visibility: hidden; }
+.shiny-output-error:before { visibility: hidden; }
+.leaflet-top, .leaflet-bottom {z-index: -10;}
+.btn {width: 69px; padding: 4px 1px;}
+.legendText {
+    font-size:50px;
+    font-family:'Arial, sans-serif';
+    font-weight: bold;
+}
+h4 {
+    margin: 0px;
+    margin-bottom: none;
+    margin-top: 8px;
+}"
+),
+
+
+div(class="outer",
+
+
+
+leafletMap("map", width="100%", height="100%",
+    # By default OpenStreetMap tiles are used
+    initialTileLayer = "//{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png",
+    initialTileLayerAttribution = NULL,
+   options=list(
+     center = c(40, -94.85),
+     zoom = 4,
+     minZoom = 4,
+     maxZoom = 4,
+     maxBounds = list(list(17, -160), list(59, -45))
+   )
+
+ ),
+
+absolutePanel(
+left = "1%", bottom = "1%", class = "socialBox", fixed = TRUE, draggable = F,
+HTML("<div style='white-space: pre; opacity: 1;'>
+<a class='twitter-share-button'
+href='http://bit.ly/1vtDKlh'
+data-url='http://bit.ly/1vtDKlh'
+data-size='large'
+data-count='none'
+data-counturl='http://bit.ly/1vtDKlh'>Tweet</a>
+<div class='fb-like'
+data-href='http://bit.ly/1vtDKlh'
+data-width='90'
+data-layout='button_count'
+data-action='like'
+ata-show-faces='true'
+data-share='true'>
+</div></div>"
+
+)
+),
+
+absolutePanel(
+right = "1.1%", bottom = ".5%", class = "socialBox", fixed = TRUE, draggable = F,
+HTML("<div style='text-align: right'>
+    <a href='http://montana.edu'><img src='MSU-horiz.png' style='height:30px; vertical-align: bottom; opacity: .7;'></a>
+    &nbsp;&nbsp;<a href='http://montanaioe.org'><img src='mus-ioe-logo-horizontal-1200w.png' style='height:28px; vertical-align: bottom;opacity: .7;'></a><br><span style='font-size: 9pt'><a href='http://leafletjs.com'>Leaflet</a> | © <a href='www.openstreetmap.org/copyright'>OpenStreetMap</a> Contributors & <a href='www.thunderforest.com'>Thunderforest</a></span></br></div>"
+)
+),
+
+    absolutePanel(
+        left = "1%", top = "1%", width = 215, class = "floater", fixed = TRUE, draggable = F,
+
+h4(tags$strong(HTML("<div style='text-align:center;'>State and National Emissions Targets</div>"))),
+uiOutput("stateGaugeOutput"),
+uiOutput("stateText2"),
+uiOutput("USGaugeOutput"),
+uiOutput("USText2"),
+tags$div(style="text-align:center; margin-top: 8px;",
+HTML("<b>Choose how to color the map</b>"),
+bsButtonGroup("btngrp1", toggle = "radio", value = "perCapita",
+bsButton("btn1", label = "Per Capita", value = "perCapita"),
+bsButton("btn2", label = "Total", value = "total"))),
+tags$div(class='row-fluid', style='margin-top: 15px',
+actionButton("reset", label = "Reset"),
+actionButton("info", label = "Info"),
+actionButton("exercise", label = "Exercises"))
+
+#uiOutput("testOut1")
+
+
+    ),
+
+
+
+
+absolutePanel(
+        right = "1%", top = "1%", width = 205, class = "floater", fixed = TRUE, draggable = F,
+
+        uiOutput("sliderTitle"),
+        HTML("<span id='commSliderTitle' style='color:darkblue; cursor:pointer; font-weight: 400;'>Commercial CO<sub>2</sub> Emissions</span>"),
+        uiOutput("commSlider"),
+        HTML("<span id='industSliderTitle' style='color: darkorange; cursor:pointer; font-weight: 400;'>Industrial CO<sub>2</sub> Emissions</span>"),
+        uiOutput("industSlider"),
+        HTML("<span id='residSliderTitle' style='color:darkgreen; cursor:pointer; font-weight: 400;'>Residential CO<sub>2</sub> Emissions</span>"),
+        uiOutput("residSlider"),
+        HTML("<span id='transSliderTitle' style='color:red; cursor:pointer; font-weight: 400;'>Transportation CO<sub>2</sub> Emissions</span>"),
+        uiOutput("transSlider"),
+        HTML("<span id='elecSliderTitle' style='color:purple; cursor:pointer; font-weight: 400;'>Electricity CO<sub>2</sub> Emissions</span>"),
+        uiOutput("elecSlider"),
+uiOutput("pieTitle"),
+showOutput("rChart", "NVD3")
+#uiOutput("stateInfo")
+#uiOutput("testOut1")
+
+    ),
+
+uiOutput("helpWindow"),
+uiOutput("exerciseWindow")
+
+
+)
+)
+)
+)
+
+})
